a686ef5f4493ddb603d8348fa4e97a3762df4f49
AchalVishnoi <vishnoiachal174@gmail.com>
2024-10-28 17:34:02 +0530

    "weight converter ready"

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
+
+     lateinit var b1:Button
+     lateinit var tv1:TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        b1=findViewById(R.id.button);
+
+        title = "KotlinApp"
+
+        b1.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
         }
+
+
     }
+
 }
--- app/src/main/java/com/example/unitconverter/tempratureConverter.kt
+++ app/src/main/java/com/example/unitconverter/tempratureConverter.kt
@@ -1,40 +1,20 @@
 package com.example.unitconverter
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.view.LayoutInflater
-import android.widget.LinearLayout
 
-class MainActivity2 : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class tempratureConverter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_temprature_converter)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
         }
-
-
-        var b1:LinearLayout=findViewById(R.id.WeightImg);
-
-        title = "KotlinApp"
-
-        b1.setOnClickListener {
-            val intent = Intent(this@MainActivity2, weightConverter::class.java)
-            startActivity(intent)
-        }
-
-
     }
 }
