ecd878656dd66e08e9dbb25af1649113a8d1bbe7
AchalVishnoi <vishnoiachal174@gmail.com>
2024-10-29 14:28:30 +0530

    "speed converter added "

---
+++ app/src/main/java/com/example/unitconverter/speedConverter.kt
@@ -0,0 +1,153 @@
+package com.example.unitconverter
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
+
+class speedConverter : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+
+    private lateinit var spinner1: Spinner
+    private lateinit var spinner2: Spinner
+    private lateinit var ep1: EditText
+    private lateinit var ep2: EditText
+    var weight = arrayOf<String?>("Meter per sec",
+        "Kilometer per hr","Foot per sec"
+    )
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_speed_converter)
+        spinner1=findViewById(R.id.spsp1)
+        spinner2=findViewById(R.id.spsp2)
+        ep1=findViewById(R.id.uppersp)
+        ep2=findViewById(R.id.Lowersp)
+        spinner1.onItemSelectedListener =this
+        spinner2.onItemSelectedListener =this
+
+
+
+        val ad: ArrayAdapter<*> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_spinner_item,
+            weight)
+        val ad2: ArrayAdapter<*> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_spinner_item,
+            weight)
+
+        ad.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item)
+        ad2.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item)
+
+        spinner1.adapter = ad
+        spinner2.adapter=ad2
+
+        ep1.doOnTextChanged {_,_,_,_->
+            if(ep1.isFocused){
+                val amt=
+                    if(ep1.text.isEmpty()) 0.0
+
+                    else ep1.text.toString().toDouble()
+
+                val convertedUnit=convertWeightUnit(amt,spinner1.selectedItem.toString(),spinner2.selectedItem.toString())
+                ep2.setText(convertedUnit.toString())
+            }
+
+        }
+        ep2.doOnTextChanged {_,_,_,_->
+            if(ep2.isFocused){
+                val amt=
+                    if(ep2.text.isEmpty()) 0.0
+                    else ep2.text.toString().toDouble()
+
+                val convertedUnit=convertWeightUnit(amt,spinner2.selectedItem.toString(),spinner1.selectedItem.toString())
+                ep1.setText(convertedUnit.toString())
+            }
+
+        }
+
+        var b1: ImageButton =findViewById(R.id.back5);
+
+        title = "KotlinApp"
+
+        b1.setOnClickListener {
+            val intent = Intent(this@speedConverter, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        when(parent!!.id){
+            R.id.sp1->{
+                val amt=
+                    if(ep1.text.isEmpty()) 0.0
+                    else ep1.text.toString().toDouble()
+
+                val convertedUnit=convertWeightUnit(amt,spinner1.selectedItem.toString(),spinner2.selectedItem.toString())
+                ep2.setText(convertedUnit.toString())
+            }
+            R.id.sp2 ->{
+                val amt=
+                    if(ep2.text.isEmpty()) 0.0
+                    else ep2.text.toString().toDouble()
+                val convertedUnit=convertWeightUnit(amt,spinner2.selectedItem.toString(),spinner1.selectedItem.toString())
+                ep1.setText(convertedUnit.toString())
+            }
+
+
+        }
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+    private fun convertWeightUnit(amt:Double, firstweight:String,secondWeight:String):Double{
+        val k=convertWeightToKilo(amt,firstweight)
+        return convertKiloToWeight(k,secondWeight)
+    }
+
+
+
+    private fun convertWeightToKilo(amt:Double, firstweight:String):Double{
+        return amt* when(firstweight){
+            "Meter per sec" -> 1.0
+            "Kilometer per hr" -> 1/3.6
+
+            else->1/3.28084
+
+
+
+        }
+    }
+    private fun convertKiloToWeight(amt:Double, secondWeight:String):Double {
+        return amt * when (secondWeight) {
+            "Meter per sec" -> 1.0
+            "Kilometer per hr" -> 3.6
+
+            else->3.28084
+
+
+        }
+    }
+
+
+}
